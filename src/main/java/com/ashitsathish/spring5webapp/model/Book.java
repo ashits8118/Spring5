d32b852da70ea42d5aff3f4b9aa3919435a35c8f
Ashit Sathish <ashitsathish@Ashits-MacBook-Pro.local>
2018-12-25 22:53:42 -0600

    Adding JPA Entites And Relationships And Joining Tables

--- src/main/java/com/ashitsathish/spring5webapp/model/Book.java
+++ src/main/java/com/ashitsathish/spring5webapp/model/Book.java
@@ -1,15 +1,21 @@
 package com.ashitsathish.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
-
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
@@ -28,7 +34,13 @@ public class Book {
         this.authors = authors;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
