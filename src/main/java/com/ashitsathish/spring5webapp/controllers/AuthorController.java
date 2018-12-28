802b01756851253fe89203a5746025937962d511
Ashit Sathish <ashitsathish@Ashits-MacBook-Pro.local>
2018-12-28 17:22:50 -0600

    added thymleaf and controllers to the project

---
+++ src/main/java/com/ashitsathish/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package com.ashitsathish.spring5webapp.controllers;
+
+import com.ashitsathish.spring5webapp.respositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", this.authorRepository.findAll());
+        return "authors";
+    }
+}
